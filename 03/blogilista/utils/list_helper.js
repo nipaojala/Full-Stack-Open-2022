684dfdabe07316456512ce56276fe3c69728d765
nipaojala <nipa.ojala@gmail.com>
2022-10-17 00:12:12 +0300

    4.4 update

--- 03/blogilista/utils/list_helper.js
+++ 03/blogilista/utils/list_helper.js
@@ -2,7 +2,13 @@ const dummy = (blogs) => {
   if (blogs) return 1
   else return null
 }
-
+const totalLikes = (blogs) => {
+  let count = 0
+  blogs.forEach(element => {
+    count = count + element.likes
+  })
+  return count
+}
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
