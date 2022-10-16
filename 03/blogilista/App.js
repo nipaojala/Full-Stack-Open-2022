bb75dc42b5e94db55f64b44933f05df3e313eb49
nipaojala <nipa.ojala@gmail.com>
2022-10-16 17:39:47 +0300

    4.2 app divided into different files

--- 03/blogilista/App.js
+++ 03/blogilista/App.js
@@ -1,1 +1,18 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+app.use(express.json())
 
+const mongoUrl = 'mongodb+srv://nipa:2Bc_productions@cluster0.gbmi2kb.mongodb.net/Blogs?retryWrites=true&w=majority'
+console.log('Connecting to ' + mongoUrl)
+mongoose.connect(mongoUrl)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
+module.exports = app
--- 03/blogilista/index.js
+++ 03/blogilista/index.js
@@ -1,37 +1,8 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+// const http = require('http')
+const app = require('./app') // varsinainen Express-sovellus
+const Blog = require('./models/blog')
 require('dotenv').config()
 const PORT = 3001
-const mongoUrl = 'mongodb+srv://nipa:2Bc_productions@cluster0.gbmi2kb.mongodb.net/Blogs?retryWrites=true&w=majority'
-console.log("Connecting to " + mongoUrl)
-mongoose.connect(mongoUrl)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
-  })
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-const Blog = mongoose.model('Blog', blogSchema)
-
-app.use(cors())
-app.use(express.json())
 
 app.get('/api/blogs', (request, response) => {
   Blog
@@ -44,13 +15,6 @@ app.get('/api/blogs', (request, response) => {
 app.post('/api/blogs', (request, response) => {
   const body = request.body
 
-  // const newBlog = new Blog({
-  //   title: body.title,
-  //   author: body.author,
-  //   url: body.url,
-  //   likes: body.likes
-  // })
-
   const blog = new Blog(body)
 
   blog
@@ -59,7 +23,7 @@ app.post('/api/blogs', (request, response) => {
       response.status(201).json(result)
     })
 })
-console.log(PORT)
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
