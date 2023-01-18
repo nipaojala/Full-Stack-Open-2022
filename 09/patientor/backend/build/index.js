5473db08cc50c2c8494108dea5d1609422ec9866
nipaojala <nipa.ojala@gmail.com>
2023-01-18 18:32:16 +0200

    9.9

--- 09/patientor/backend/build/index.js
+++ 09/patientor/backend/build/index.js
@@ -4,13 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const diaries_1 = __importDefault(require("./routes/diaries"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+const cors_1 = __importDefault(require("cors"));
+app.use((0, cors_1.default)());
 const PORT = 3000;
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
 });
+app.use('/api/diaries', diaries_1.default);
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
