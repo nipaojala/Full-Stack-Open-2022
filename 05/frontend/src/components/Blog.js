6d8a054344bccacca3b14bf9e4de5a40eb9b4373
nipaojala <nipa.ojala@gmail.com>
2023-02-08 16:14:01 +0200

    little changes to visibility to make test eligible

--- 05/frontend/src/components/Blog.js
+++ 05/frontend/src/components/Blog.js
@@ -1,6 +1,16 @@
 import Togglable from "./Togglable"
+import { useState } from "react"
 
 const Blog = ({blog, handleLikeCount, handleDelete}) => {
+  const [visibility, setVisibility] = useState(false)
+
+  const hideWhenVisible = { display: visibility ? 'none' : '' }
+  const showWhenVisible = { display: visibility ? '' : 'none'}
+
+  const toggleVisibility = () => {
+    setVisibility(!visibility)
+  }
+
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
@@ -11,14 +21,18 @@ const Blog = ({blog, handleLikeCount, handleDelete}) => {
   }
   return (
   <div style={blogStyle}>
-    {blog.title}
-    <Togglable buttonLabel="show">
-      {blog.author}<br/>
-      {blog.url}<br/>
-      {blog.likes}<button onClick={() => handleLikeCount(blog)}>like</button><br/>
-      {blog.user.name}<br/>
+      <div style = {hideWhenVisible}>
+      {blog.title}<br/>
+      <button type = 'button' id = 'view-button' onClick = {toggleVisibility}> view </button>
+      </div>
+      <div style = {showWhenVisible}>
+        {blog.title} {blog.author}
+        <button type = 'button' onClick = {toggleVisibility}> hide </button>
+        <br/>{blog.url}<br/>
+        {blog.likes}<button onClick={() => handleLikeCount(blog)}>like</button><br/>
+        {blog.user.name}<br/>
       <button onClick={() => handleDelete(blog)}>remove</button><br/>
-    </Togglable>
+      </div>
   </div>  
   )
 }
--- 05/frontend/src/components/Blog.test.js
+++ 05/frontend/src/components/Blog.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 
 test('renders content', () => {
   
@@ -18,3 +19,28 @@ test('renders content', () => {
   const element = screen.getByText('Component testing is done with react-testing-library', 'test')
   expect(element).toBeDefined()
 })
+
+// test('clicking the button calls event handler once', async () => {
+//   const blog = {
+//     title: 'Component testing is done with react-testing-library',
+//     author: 'Niilo',
+//     url: 'url',
+//     likes: 8,
+//     user: {
+//       name: 'Niilo'
+//     }
+//   }
+
+//   const mockHandler = jest.fn()
+
+//   render(
+//     <Blog blog={blog} />
+//   )
+
+//   // const user = userEvent.setup()
+//   // const button = screen.getByText('show')
+//   // await user.click(button)
+
+//   const element = screen.getByText('Niilo', 'url')
+//   expect(element).toBeDefined()
+// })
